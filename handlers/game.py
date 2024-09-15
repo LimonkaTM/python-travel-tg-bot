01c13b3264684e2f400e4952791e4891ff3e8acc
LimonkaTM <etuzin@mail.ru>
2024-09-15 18:23:51 +0300

    Добавил функцию отправки первого вопроса

--- handlers/game.py
+++ handlers/game.py
@@ -1,10 +1,13 @@
 from aiogram import F
 from aiogram.dispatcher.router import Router
 from aiogram.types import CallbackQuery, InputMediaPhoto, FSInputFile
+from aiogram.fsm.context import FSMContext
 
-from loader import bot
+from states.game_state import gameState
 
-from keyboards.game import create_start_game_kb
+from loader import bot, game_data
+
+from keyboards.game import create_start_game_kb, create_question_kb
 
 
 router: Router = Router(name='gameRouter')
@@ -23,5 +26,28 @@ async def send_start_game_msg(callback: CallbackQuery) -> None:
                                  message_id=callback.message.message_id,
                                  media=photo,
                                  reply_markup=create_start_game_kb())
+
+    return None
+
+
+@router.callback_query(F.data == 'start_game')
+async def send_game_question_msg(callback: CallbackQuery, state: FSMContext) -> None:
+    '''
+    Отправляет вопрос
+    '''
+
+    await callback.message.delete()
+
+    await state.set_state(gameState.question_index)
+    await state.update_data(question_index=0)
+    await state.update_data(score=0)
+
+    question = game_data[0]['question']
+
+    await bot.send_message(
+        chat_id=callback.message.chat.id,
+        text=question,
+        reply_markup=create_question_kb(0)
+    )
 
     return None
