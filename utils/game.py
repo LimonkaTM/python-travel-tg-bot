8b2ce0611d1f31978deb9a018440aab0a1426602
LimonkaTM <etuzin@mail.ru>
2024-09-15 20:39:28 +0300

    Изменил логику расчета результатов игры

--- utils/game.py
+++ utils/game.py
@@ -1,5 +1,13 @@
-def is_passed(score: int, question_count: int) -> bool:
+def calc_percent_true_answers(score: int, question_count: int) -> int:
     percents = round((100 * score) / question_count, 0)
-    print(percents)
 
-    return int(percents) >= 80
+    return int(percents)
+
+
+def define_type_game_result(score: str) -> str:
+    if score >= 80:
+        return 'pro'
+    elif score > 50 & score < 80:
+        return 'middle'
+    else:
+        return 'junior'
